8efc6fa59653a606ae8c9e711d09f7f1d08453a6
Joy Williams <joyful@anniecannons.com>
2019-09-19 11:35:17 -0700

    dashInsert Final

--- dashInsert.js
+++ dashInsert.js
@@ -22,9 +22,24 @@ console.log(result) // "0"
 
 var result = isBigger("13");
 console.log(result) // "1-3"
-
+create a variable to store the transformation of number first to a string and then to array
+output a string with dashes between the odd numbers
+create a variable to store the transformation of the number to an array of numbers
+by first turning into a string, tools: toString().split("")
+create for loop. need a conditional statemnet that will test if the number is odd as well as && if the number after is odd
+if true I want to add the number to the string and the dash
+if false I want to just add the number 
 **/
 
-function dashInsert(str) {
-	// Your code
+function dashInsert(num) {
+	var out="";
+	var array=num.toString().split("");
+	for (var i=0; i<array.length;i++){
+		if (array [i] %2===1 && array[i+1] % 2===1)
+	}output =output + array[i] + "-";
+}else{
+	output=output + array[i];
 }
+return output;// varYour code
+}
+dashInsert(34567);
